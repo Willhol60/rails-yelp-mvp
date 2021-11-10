633c67e7832f4a0ebf60c6562fed62710d56b7fd
Will Holmes <willholmes60@gmail.com>
2021-11-10 12:50:35 +0000

    built models and controllers, working on views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::String.random(length: 10),
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "added a restaurant called #{restaurant.name}"
+end
